ac0f0849f23943bd700b341854778f1aa16b1f4a
Andy Acosta <andyacosta@andys-mbp.local>
2016-11-09 18:13:00 -0500

    updated getraw.cpp

---
+++ myo/sdk/collectraw/collectraw/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  collectraw
+//
+//  Created by Andy Acosta on 11/8/16.
+//  Copyright © 2016 Andy Acosta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
