b3b63f340fff9a2bf1161254f11b783f18563187
Andy Acosta <andyacosta@andys-mbp.local>
2016-12-13 22:08:19 -0500

    removed random myo folder

--- myo/sdk/project/grt/build/tmp/third_party/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aacosta/Documents/seniordesign/sd-2017-asl-keyword-to-sentence/myo/sdk/project/grt/third_party/gtest/googletest
-# Build directory: /Users/aacosta/Documents/seniordesign/sd-2017-asl-keyword-to-sentence/myo/sdk/project/grt/build/tmp/third_party/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
